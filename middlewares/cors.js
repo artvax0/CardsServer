0a586bdce10b83345b8335cd1cf2d8fa0cdac2a8
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-24 14:00:04 +0300

    Update CORS

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const mwCors = cors({
-  origin: ['http://127.0.0.1:5500', 'www.grimbirb.com'],
+  origin: ['http://127.0.0.1:5500', 'www.grimbirb.com', 'http://localhost:5173'],
 })
 
 export default mwCors;
