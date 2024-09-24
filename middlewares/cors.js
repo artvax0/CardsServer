d1a12d82057cf7bddb2835cb0deef66a136f759f
artvax0 <168709894+artvax0@users.noreply.github.com>
2024-09-24 14:51:22 +0300

    Update CORS to have Static Website

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,7 +1,7 @@
 import cors from 'cors';
 
 const mwCors = cors({
-  origin: ['http://127.0.0.1:5500', 'http://localhost:5173'],
+  origin: ['http://127.0.0.1:5500', 'http://localhost:5173', 'https://cards-project-38ja.onrender.com'],
 })
 
 export default mwCors;
